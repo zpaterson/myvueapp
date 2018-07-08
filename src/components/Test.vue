2172af9aebb258215cc714c2a0eb15de001b0400
Zaire Paterson <zzpaterson@gmail.com>
2018-07-07 21:42:05 -0700

    added toggle for showing name in test file

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,12 +1,27 @@
 <template>
   <div class="test">
-     <h1>Test</h1>
+     <h1>{{title}}</h1>
+     <!-- <span v-html="title"></span> -->
+     <p v-if="showName">{{user.firstName}}</p>
+     <p v-else>You don't have a name</p>
+     <!-- <p v-text="user.lastName"></p> -->
   </div>
 </template>
 
 <script>
     export default {
-       name: 'test' 
+       name: 'test',
+       data() {
+          return {
+            title: 'Hello World',
+            // title: '<h1>Hello Worlds</h1>',
+            user: {
+              firstName: 'Zaire',
+              lastName: 'Paterson'
+            },
+            showName: true
+          }
+       } 
     }
 </script>
 
