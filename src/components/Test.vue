95aa3fa8749404e5a1617027618d403ba29c57e3
Zaire Paterson <zzpaterson@gmail.com>
2018-07-07 21:17:55 -0700

    added dependencies and test file

---
+++ src/components/Test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="test">
+     <h1>Test</h1>
+  </div>
+</template>
+
+<script>
+    export default {
+       name: 'test' 
+    }
+</script>
+
+<style scoped>
+
+</style>
+
