ceb969ae274642f2bbc65cd2d42b8eb9c1f8bc85
Zaire Paterson <zzpaterson@gmail.com>
2018-07-07 22:19:15 -0700

    implemented av-for list items and a v-model for a dynamic input text box

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="test">
+      <input type="text" v-model="title">
+      <br />
      <h1>{{title}}</h1>
      <!-- <span v-html="title"></span> -->
      <p v-if="showName">{{user.firstName}}</p>
      <p v-else>You don't have a name</p>
      <!-- <p v-text="user.lastName"></p> -->
+     <ul>
+       <li v-for="item in items" v-bind:key="item">{{item.title}}</li>
+     </ul>
   </div>
 </template>
 
@@ -19,7 +24,12 @@
               firstName: 'Zaire',
               lastName: 'Paterson'
             },
-            showName: true
+            showName: true,
+            items: [
+              {title: 'Item One'},
+              {title: 'Item Two'},
+              {title: 'Item Three'},
+            ]
           }
        } 
     }
